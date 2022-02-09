9d571a3af3c39ff02e5cf003a0821c9b62fd9ed7
Русинов Дмитрий <drusinov@neoflex.ru>
2022-02-09 18:52:05 +0300

    initial commit

---
+++ pages/_app.tsx
@@ -0,0 +1,44 @@
+import cookie from 'cookie'
+import * as React from 'react'
+import type { IncomingMessage } from 'http'
+import type { AppProps, AppContext } from 'next/app'
+
+import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr'
+
+const keycloakCfg = {
+  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || "",
+  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "",
+  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_ID || "",
+}
+
+interface InitialProps {
+  cookies: unknown
+}
+
+function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
+
+  return (
+    <SSRKeycloakProvider
+      keycloakConfig={keycloakCfg}
+      persistor={SSRCookies(cookies)}
+    >
+      <Component {...pageProps} />
+    </SSRKeycloakProvider>
+  )
+}
+
+function parseCookies(req?: IncomingMessage) {
+  if (!req || !req.headers) {
+    return {}
+  }
+  return cookie.parse(req.headers.cookie || '')
+}
+
+MyApp.getInitialProps = async (context: AppContext) => {
+  // Extract cookies from AppContext
+  return {
+    cookies: parseCookies(context?.ctx?.req),
+  }
+}
+
+export default MyApp
