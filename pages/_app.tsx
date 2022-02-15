72c1f4a0e449282ffc5ad354ad535181633c011b
Русинов Дмитрий <drusinov@neoflex.ru>
2022-02-15 23:33:55 +0300

    prettier

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,51 +1,55 @@
 import cookie from 'cookie'
 import * as React from 'react'
-import type { IncomingMessage } from 'http'
-import type { AppProps, AppContext } from 'next/app'
+import type {IncomingMessage} from 'http'
+import type {AppProps, AppContext} from 'next/app'
 
-import { SSRKeycloakProvider, SSRCookies } from '@react-keycloak/ssr'
-import { appWithTranslation } from 'next-i18next'
+import {SSRKeycloakProvider, SSRCookies} from '@react-keycloak/ssr'
+import {appWithTranslation} from 'next-i18next'
 
 const keycloakCfg = {
-  url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || "",
-  realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "",
-  clientId: process.env.NEXT_PUBLIC_KEYCLOAK_ID || "",
+    url: process.env.NEXT_PUBLIC_KEYCLOAK_URL || "",
+    realm: process.env.NEXT_PUBLIC_KEYCLOAK_REALM || "",
+    clientId: process.env.NEXT_PUBLIC_KEYCLOAK_ID || "",
 }
 
 interface InitialProps {
-  cookies: unknown
+    cookies: unknown
 }
 
 
-function MyApp({ Component, pageProps, cookies }: AppProps & InitialProps) {
+function MyApp({
+                   Component,
+                   pageProps,
+                   cookies
+               }: AppProps & InitialProps) {
 
-  return (
-    <SSRKeycloakProvider
-      keycloakConfig={keycloakCfg}
-      persistor={SSRCookies(cookies)}
-    >
-      <Component {...pageProps} />
-    </SSRKeycloakProvider>
-  )
+    return (
+        <SSRKeycloakProvider
+            keycloakConfig={keycloakCfg}
+            persistor={SSRCookies(cookies)}
+        >
+            <Component {...pageProps} />
+        </SSRKeycloakProvider>
+    )
 }
 
 function parseCookies(req?: IncomingMessage) {
-  if (!req || !req.headers) {
-    return {}
-  }
-  return cookie.parse(req.headers.cookie || '')
+    if (!req || !req.headers) {
+        return {}
+    }
+    return cookie.parse(req.headers.cookie || '')
 }
 
 MyApp.getInitialProps = async ({Component, ctx}: AppContext) => {
-  let pageProps = {}
-  if (Component?.getInitialProps) {
-    pageProps = await Component.getInitialProps(ctx)
-  }
-  return {
-    // Extract cookies from AppContext
-    cookies: parseCookies(ctx?.req),
-    pageProps
-  }
+    let pageProps = {}
+    if (Component?.getInitialProps) {
+        pageProps = await Component.getInitialProps(ctx)
+    }
+    return {
+        // Extract cookies from AppContext
+        cookies: parseCookies(ctx?.req),
+        pageProps
+    }
 }
 
 export default appWithTranslation(MyApp)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,61 +1,61 @@
-import { useKeycloak } from '@react-keycloak/ssr'
-import type { KeycloakInstance, KeycloakTokenParsed } from 'keycloak-js'
+import {useKeycloak} from '@react-keycloak/ssr'
+import type {KeycloakInstance, KeycloakTokenParsed} from 'keycloak-js'
 import Link from 'next/link'
-import { Layout } from '../components/Layout'
+import {Layout} from '../components/Layout'
 import {serverSideTranslations} from "next-i18next/serverSideTranslations";
 import {useTranslation} from "next-i18next";
 import {useRouter} from "next/router";
 
 type ParsedToken = KeycloakTokenParsed & {
-  email?: string
+    email?: string
 
-  preferred_username?: string
+    preferred_username?: string
 
-  given_name?: string
+    given_name?: string
 
-  family_name?: string
+    family_name?: string
 }
 
 const IndexPage = () => {
-  const { keycloak } = useKeycloak<KeycloakInstance>()
-  const parsedToken: ParsedToken | undefined = keycloak?.tokenParsed
+    const {keycloak} = useKeycloak<KeycloakInstance>()
+    const parsedToken: ParsedToken | undefined = keycloak?.tokenParsed
     const router = useRouter()
     const {t} = useTranslation('common')
-  const loggedinState = keycloak?.authenticated ? (
-    <span className="text-success">logged in</span>
-  ) : (
-    <span className="text-danger">NOT logged in</span>
-  )
+    const loggedinState = keycloak?.authenticated ? (
+        <span className="text-success">logged in</span>
+    ) : (
+        <span className="text-danger">NOT logged in</span>
+    )
 
-  const welcomeMessage =
-    keycloak?.authenticated || (keycloak && parsedToken)
-      ? t("welcome-logged", {userName: parsedToken?.preferred_username ?? ''})
-      : t("welcome-anon")
+    const welcomeMessage =
+        keycloak?.authenticated || (keycloak && parsedToken)
+            ? t("welcome-logged", {userName: parsedToken?.preferred_username ?? ''})
+            : t("welcome-anon")
 
-  return (
-    <Layout title={t('h1')}>
-      <h1 className="mt-5">{t('h1')}</h1>
-      <div className="mb-5 lead text-muted">
-          {t('h2')}
-      </div>
-        <Link
-            href='/'
-            locale={router.locale === 'en' ? 'ru' : 'en'}
-        >
-            <button>
-                {t('locale-button')}
-            </button>
-        </Link>
-      <p> {t('you-are')}: {loggedinState}</p>
-      <p>{welcomeMessage}</p>
-    </Layout>
-  )
+    return (
+        <Layout title={t('h1')}>
+            <h1 className="mt-5">{t('h1')}</h1>
+            <div className="mb-5 lead text-muted">
+                {t('h2')}
+            </div>
+            <Link
+                href='/'
+                locale={router.locale === 'en' ? 'ru' : 'en'}
+            >
+                <button>
+                    {t('locale-button')}
+                </button>
+            </Link>
+            <p> {t('you-are')}: {loggedinState}</p>
+            <p>{welcomeMessage}</p>
+        </Layout>
+    )
 }
 
 export const getStaticProps = async ({locale}: { locale: string; }) => ({
-        props: {
-            ...(await serverSideTranslations(locale, ['common']))
-        }
-    })
+    props: {
+        ...(await serverSideTranslations(locale, ['common']))
+    }
+})
 
 export default IndexPage
